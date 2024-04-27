092132128914c71675233dc0d114b92e55ccdde0
TesakDev <57626839+TesakDev@users.noreply.github.com>
2024-04-27 16:43:56 +0300

    init

---
+++ TestTaskForETAI__CategoriesService/Entity/Category.cs
@@ -0,0 +1,14 @@
+﻿using LinqToDB.Mapping;
+
+namespace TestTaskForETAI__CategoriesService.Entity;
+
+[Table(Name = "Categories")]
+public class Category
+{
+    [PrimaryKey, Identity] public int Id { get; set; }
+    [Column] public string Name { get; set; } = null!;
+    [Column] public int? ParentCategoryId { get; set; }
+    [Association(ThisKey = "Id", OtherKey = "ParentCategoryId")]
+    public List<Category> SubCategories { get; set; } = new List<Category>();
+
+}
---
+++ TestTaskForETAI__CategoriesService/Entity/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using LinqToDB;
+using LinqToDB.Data;
+
+namespace TestTaskForETAI__CategoriesService.Entity;
+
+public class DbContext : DataConnection
+{
+    public DbContext(IConfiguration configuration) : base(configuration.GetConnectionString("Db"))
+    {
+    }
+    public ITable<Category> Categories => this.GetTable<Category>();
+}
